498157a47644a4bb4216d063215cfdbea8db78d9
Oksana Omelchuk <oksana22022001@gmail.com>
2020-09-14 21:48:09 +0300

    project2

---
+++ project2/project2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project2
+{
+    class Program
+    {
+        public static int Main()
+        {
+            Console.Write("Введіть кінець діапазону для виведення ряду Фібоначчі від 1 до : ");
+            int end = int.Parse(Console.ReadLine());
+            int j = 1;
+            for (int k = 1; k <= end; k += j)
+            {
+                Console.Write("{0} ", k);
+                j = k - j;
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
